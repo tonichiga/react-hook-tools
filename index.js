3cdd33f32174f8b508d73ead8502db11216bec9f
Anton Chyhariev <tonichiga@gmail.com>
2021-12-11 14:11:34 +0200

    added new hook useZoom

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export { default as useScale } from "./hooks/useScale";
-export { default as useZoon } from "./hooks/useZoon";
+export { default as useZoom } from "./hooks/useZoom";
 export { default as useClickOutside } from "./hooks/useClickOutside";
 export { default as useSubmitOnEnter } from "./hooks/useSubmitOnEnter";
