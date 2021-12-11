bd1cabf2a9c72052d56c36a5f43798a417b3cb27
Anton Chyhariev <tonichiga@gmail.com>
2021-12-11 12:17:54 +0200

    added submit hook

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export { default as useScale } from "./hooks/useScale";
 export { default as useClickOutside } from "./hooks/useClickOutside";
+export { default as useSubmitOnEnter } from "./hooks/useSubmitOnEnter";
