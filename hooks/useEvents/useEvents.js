ec9fb465a7256884f14bcfe1c64952cc7beefaba
Toni <tonichiga@gmail.com>
2022-02-17 11:10:23 +0200

    added log features

--- hooks/useEvents/useEvents.js
+++ hooks/useEvents/useEvents.js
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
 import { storageBase } from "./storage";
 
-const useEvents = (isRegistered = false) => {
+const useEvents = (isRegistered = {}) => {
   useEffect(() => {
     alt.on("event", (event, ...data) => {
+      isRegistered.entry && console.log("[F] Entry event", event);
+      isRegistered.data && console.log("[F] Entry event", JSON.stringify(data));
+
       if (storageBase[event] !== undefined) {
-        isRegistered &&
-          console.log("EVENT IN STORAGE", JSON.stringify(storageBase[event]));
+        isRegistered.found && console.log("[F] Event found! IN STORAGE", event);
         storageBase[event](...data);
       } else {
-        isRegistered && console.log("EVENT NOT FOUND", event);
+        isRegistered.notFound &&
+          console.log("[F] Event not found! IN STORAGE", event);
       }
     });
 
