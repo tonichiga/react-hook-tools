767606de4503585a14357524b9c7d9255b278e12
Антон Чигарев <tonichiga@gmail.com>
2022-10-20 22:04:20 +0300

    support multi refs

--- hooks/useClickOutside.js
+++ hooks/useClickOutside.js
@@ -4,9 +4,20 @@ const useClickOutside = (ref = [], callback, event = "mousedown") => {
   useEffect(() => {
     function handleClickOutside(event) {
       if (Array.isArray(ref)) {
-        ref.forEach((r) => {
-          if (r.current && !r.current.contains(event.target)) {
-            callback();
+        ref.forEach(({ current }) => {
+          if (Array.isArray(current)) {
+            if (
+              current[0] &&
+              current[1] &&
+              !current[0].contains(event.target) &&
+              !current[1].contains(event.target)
+            ) {
+              closeModals();
+            }
+          } else {
+            if (current && !current.contains(event.target)) {
+              closeModals();
+            }
           }
         });
       } else {
