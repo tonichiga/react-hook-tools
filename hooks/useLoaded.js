cec03a284634a5462b7bfba343465e0a0740ebe9
Toni <tonichiga@gmail.com>
2024-08-08 12:32:42 +0300

    extnd hook

--- hooks/useLoaded.js
+++ hooks/useLoaded.js
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import uuid from "react-uuid";
 
-const useLoaded = ({ defaultLoadingState = false }) => {
-  const [isLoading, setIsLoading] = useState(defaultLoadingState);
+const useLoaded = (
+  props = {
+    defaultLoadingState: false,
+    enableTimeout: false,
+  }
+) => {
+  const [isLoading, setIsLoading] = useState(
+    props?.defaultLoadingState || false
+  );
+  const timeout = useRef(null);
 
   const emitFetching = (callback) => {
     setIsLoading(true);
-    callback?.();
+    callback?.(uuid());
+
+    if (!props.enableTimeout) return;
+    clearTimeout(timeout.current);
+    timeout.current = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
   };
 
   const emitLoaded = (callback) => {
     setIsLoading(false);
-    callback?.();
+    callback?.(uuid());
+    clearTimeout(timeout.current);
   };
 
   return {
